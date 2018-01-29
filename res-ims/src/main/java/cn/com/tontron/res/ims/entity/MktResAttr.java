cfbf92550260e8c25c77f25dfca8dc7339bf7662
yangyang <younker163@163.com>
2018-01-29 17:03:31 +0800

    添加 营销资源属性

---
+++ res-ims/src/main/java/cn/com/tontron/res/ims/entity/MktResAttr.java
@@ -0,0 +1,59 @@
+package cn.com.tontron.res.ims.entity;
+
+import cn.com.tontron.res.common.base.entity.MktModel;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by yangyang on 2018/1/29.
+ */
+@Table(name = "ims_attr")
+@Entity
+public class MktResAttr extends MktModel {// 营销资源属性
+    private Long mktResAttrId; // 营销资源属性 标识:记录营销资源属性标识。
+    private Long mktResId; // 营销资源标识: 记录营销资源标识
+    private Long attrId; // 属性标识: 记录属性标识
+    private String defaultValue; // 默认值
+    private String applyRegionId; // 适用区域标识: 记录适用区域标识
+
+    public Long getMktResAttrId() {
+        return mktResAttrId;
+    }
+
+    public void setMktResAttrId(Long mktResAttrId) {
+        this.mktResAttrId = mktResAttrId;
+    }
+
+    public Long getMktResId() {
+        return mktResId;
+    }
+
+    public void setMktResId(Long mktResId) {
+        this.mktResId = mktResId;
+    }
+
+    public Long getAttrId() {
+        return attrId;
+    }
+
+    public void setAttrId(Long attrId) {
+        this.attrId = attrId;
+    }
+
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+    public String getApplyRegionId() {
+        return applyRegionId;
+    }
+
+    public void setApplyRegionId(String applyRegionId) {
+        this.applyRegionId = applyRegionId;
+    }
+}
