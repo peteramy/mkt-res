c0ade50ac309afba9827cfaa88a61321d554b0a1
yangyang <younker163@163.com>
2018-01-29 16:35:24 +0800

    调整实体类继承

--- res-inv/src/main/java/cn/com/tontron/res/ivms/entity/MktResInvUseRec.java
+++ res-inv/src/main/java/cn/com/tontron/res/ivms/entity/MktResInvUseRec.java
@@ -1,5 +1,6 @@
 package cn.com.tontron.res.ivms.entity;
 
+import cn.com.tontron.res.common.base.entity.MktModel;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
@@ -10,59 +11,40 @@ import java.util.Date;
  */
 @Table(name = "inv_use_rec")
 @Entity
-public class MktResInvUseRec { //营销资源发票使用记录
-    @Length(max = 10)
-    private String actType; //通用数据操作类型：通用数据操作类型, KIP=保持/ADD=新增 /MOD=修改/DEL=删除
+public class MktResInvUseRec extends MktModel { //营销资源发票使用记录
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long mktResUseRecId; //发票使用记录标识：营销资源使用记录 ID 的标识，主键
-    private long mktResInvInstId; //营销资源发票实例标识：记录营销资源实例 ID。
+    private Long mktResUseRecId; //发票使用记录标识：营销资源使用记录 ID 的标识，主键
+    private Long mktResInvInstId; //营销资源发票实例标识：记录营销资源实例 ID。
     @Length(max = 3)
     private String useSystem; //发票使用系统：记录使用发票的系统 或模块:如计费，自 助终端等。 2015.04.21
     @Length(max = 30)
     private String printNbr; //发票打印流水：记录计费或其他系统打印发票的流水号
     @Length(max = 6)
     private String usedCd; //发票使用状态：记录发票使用的状态:发票打印成功， 未打印发票作废，已 打印普票作废，已打 印增值税专票作废。 LOVB=RES-C-0028
-    private long invStaNum; //起始票号：记录当前打印或作废的起始票号。一般需要记录每一张打印或作废的发票，起始票号和结束票号相同;如果是发票未打印作废，一般都是一本发票过期，需要分别记录开始和结束票号。 20150421
-    private long invEndNum; //结束票号：记录当前打印或作废的结束票号。一般需要记录每一张打印或作废的发票，起始票号和结束票号相同;如果是发票未打印作废，一般都是一本发票过期，需要分别记录开始和结束票号。
+    private Long invStaNum; //起始票号：记录当前打印或作废的起始票号。一般需要记录每一张打印或作废的发票，起始票号和结束票号相同;如果是发票未打印作废，一般都是一本发票过期，需要分别记录开始和结束票号。 20150421
+    private Long invEndNum; //结束票号：记录当前打印或作废的结束票号。一般需要记录每一张打印或作废的发票，起始票号和结束票号相同;如果是发票未打印作废，一般都是一本发票过期，需要分别记录开始和结束票号。
     private Date usingTime; //使用时间：记录使用时间。
-    private long usedQuantity; //使用数量：记录使用数量。
-    @Length(max = 2000)
-    private String remark; //备注：记录备注信息，如发票作废原因，使用记录无效原因。
-    private long mktResEventId; //营销资源库存变动事件标识：营销资源库存变动事件标识，记录申请发票作废事件标识
-    @Length(max = 10)
-    private String statusCd; //状态：记录状态。LOVB=PUB-C-0001。
-    private Date statusDate; //状态时间：记录状态变更的时间。
-    private long createPost; //创建岗位：记录首次创建的系统岗位标识。
-    private long createOrgId; //创建组织标识：记录首次创建的组织机构标识。
-    private long createStaff; //创建人：记录首次创建的员工标识。
-    private Date createDate; //创建时间：记录首次创建的时间。
-    private long updateStaff; //修改人：记录每次修改的员工标识。
-    private Date updateDate; //修改时间：记录每次修改的时间。
-    private long lanId; //本地网标识：本地网标识。
-    private long regionId; //区域标识：指向公共管理区域标识
+    private Long usedQuantity; //使用数量：记录使用数量。
+    private Long mktResEventId; //营销资源库存变动事件标识：营销资源库存变动事件标识，记录申请发票作废事件标识
+    private Long createPost; //创建岗位：记录首次创建的系统岗位标识。
+    private Long createOrgId; //创建组织标识：记录首次创建的组织机构标识。
+    private Long lanId; //本地网标识：本地网标识。
+    private Long regionId; //区域标识：指向公共管理区域标识
 
-    public String getActType() {
-        return actType;
-    }
-
-    public void setActType(String actType) {
-        this.actType = actType;
-    }
-
-    public long getMktResUseRecId() {
+    public Long getMktResUseRecId() {
         return mktResUseRecId;
     }
 
-    public void setMktResUseRecId(long mktResUseRecId) {
+    public void setMktResUseRecId(Long mktResUseRecId) {
         this.mktResUseRecId = mktResUseRecId;
     }
 
-    public long getMktResInvInstId() {
+    public Long getMktResInvInstId() {
         return mktResInvInstId;
     }
 
-    public void setMktResInvInstId(long mktResInvInstId) {
+    public void setMktResInvInstId(Long mktResInvInstId) {
         this.mktResInvInstId = mktResInvInstId;
     }
 
@@ -90,19 +72,19 @@ public class MktResInvUseRec { //营销资源发票使用记录
         this.usedCd = usedCd;
     }
 
-    public long getInvStaNum() {
+    public Long getInvStaNum() {
         return invStaNum;
     }
 
-    public void setInvStaNum(long invStaNum) {
+    public void setInvStaNum(Long invStaNum) {
         this.invStaNum = invStaNum;
     }
 
-    public long getInvEndNum() {
+    public Long getInvEndNum() {
         return invEndNum;
     }
 
-    public void setInvEndNum(long invEndNum) {
+    public void setInvEndNum(Long invEndNum) {
         this.invEndNum = invEndNum;
     }
 
@@ -114,107 +96,51 @@ public class MktResInvUseRec { //营销资源发票使用记录
         this.usingTime = usingTime;
     }
 
-    public long getUsedQuantity() {
+    public Long getUsedQuantity() {
         return usedQuantity;
     }
 
-    public void setUsedQuantity(long usedQuantity) {
+    public void setUsedQuantity(Long usedQuantity) {
         this.usedQuantity = usedQuantity;
     }
 
-    public String getRemark() {
-        return remark;
-    }
-
-    public void setRemark(String remark) {
-        this.remark = remark;
-    }
-
-    public long getMktResEventId() {
+    public Long getMktResEventId() {
         return mktResEventId;
     }
 
-    public void setMktResEventId(long mktResEventId) {
+    public void setMktResEventId(Long mktResEventId) {
         this.mktResEventId = mktResEventId;
     }
 
-    public String getStatusCd() {
-        return statusCd;
-    }
-
-    public void setStatusCd(String statusCd) {
-        this.statusCd = statusCd;
-    }
-
-    public Date getStatusDate() {
-        return statusDate;
-    }
-
-    public void setStatusDate(Date statusDate) {
-        this.statusDate = statusDate;
-    }
-
-    public long getCreatePost() {
+    public Long getCreatePost() {
         return createPost;
     }
 
-    public void setCreatePost(long createPost) {
+    public void setCreatePost(Long createPost) {
         this.createPost = createPost;
     }
 
-    public long getCreateOrgId() {
+    public Long getCreateOrgId() {
         return createOrgId;
     }
 
-    public void setCreateOrgId(long createOrgId) {
+    public void setCreateOrgId(Long createOrgId) {
         this.createOrgId = createOrgId;
     }
 
-    public long getCreateStaff() {
-        return createStaff;
-    }
-
-    public void setCreateStaff(long createStaff) {
-        this.createStaff = createStaff;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public long getUpdateStaff() {
-        return updateStaff;
-    }
-
-    public void setUpdateStaff(long updateStaff) {
-        this.updateStaff = updateStaff;
-    }
-
-    public Date getUpdateDate() {
-        return updateDate;
-    }
-
-    public void setUpdateDate(Date updateDate) {
-        this.updateDate = updateDate;
-    }
-
-    public long getLanId() {
+    public Long getLanId() {
         return lanId;
     }
 
-    public void setLanId(long lanId) {
+    public void setLanId(Long lanId) {
         this.lanId = lanId;
     }
 
-    public long getRegionId() {
+    public Long getRegionId() {
         return regionId;
     }
 
-    public void setRegionId(long regionId) {
+    public void setRegionId(Long regionId) {
         this.regionId = regionId;
     }
 }
