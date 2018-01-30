6cdb2a4e4fdee433d65f4457570ca89747c8a55b
ghg54321 <568494076@qq.com>
2018-01-30 11:12:46 +0800

    3.2.1. 营销资源类别详细信息

---
+++ res-ims/src/main/java/cn/com/tontron/res/ims/dto/MktResTypeDetail.java
@@ -0,0 +1,16 @@
+package cn.com.tontron.res.ims.dto;
+
+import cn.com.tontron.res.ims.entity.MktResType;
+import cn.com.tontron.res.ims.entity.MktResTypeAttr;
+import cn.com.tontron.res.ims.entity.MktResTypeRegion;
+
+import java.util.List;
+
+/**
+ * Created by yyn on 2018/1/30.
+ * 营销资源类别详细信息，包含营销资源类别主表对象、营销资源类别属性表对象 及营销资源类别适用区域表对象。
+ */
+public class MktResTypeDetail extends MktResType { //3.2.1. 营销资源类别详细信息
+    private List<MktResTypeAttr> mktResTypeAttrs;
+    private List<MktResTypeRegion>
+}
