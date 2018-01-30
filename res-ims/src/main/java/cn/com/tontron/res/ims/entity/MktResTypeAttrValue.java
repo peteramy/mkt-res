c7d9c4724418f64f19f06c8e08f9c250d13b3d01
yangyang <younker163@163.com>
2018-01-30 10:54:14 +0800

    添加 营销资源类别属性选用值

---
+++ res-ims/src/main/java/cn/com/tontron/res/ims/entity/MktResTypeAttrValue.java
@@ -0,0 +1,53 @@
+package cn.com.tontron.res.ims.entity;
+
+import cn.com.tontron.res.common.base.entity.MktModel;
+
+import javax.persistence.*;
+
+/**
+ * Created by yangyang on 2018/1/30.
+ * 3.1.44. 营销资源类别属性选用值
+ * 记录营销营销资源类别属性选用值
+ */
+@Table(name = "ims_type_attr_value")
+@Entity
+public class MktResTypeAttrValue extends MktModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long mktResAttrValId; // 营销资源属 性选用值标 识: 记录营销资源属性选用值 标识
+    private Long mktResTypeAttrId; // 营销资源类 别属性标识: 记录营销资源类别属性标 识
+    private Long attrValueId; // 属性值标识
+    private Long applyRegionId; // 适用区域标 识
+
+    public Long getMktResAttrValId() {
+        return mktResAttrValId;
+    }
+
+    public void setMktResAttrValId(Long mktResAttrValId) {
+        this.mktResAttrValId = mktResAttrValId;
+    }
+
+    public Long getMktResTypeAttrId() {
+        return mktResTypeAttrId;
+    }
+
+    public void setMktResTypeAttrId(Long mktResTypeAttrId) {
+        this.mktResTypeAttrId = mktResTypeAttrId;
+    }
+
+    public Long getAttrValueId() {
+        return attrValueId;
+    }
+
+    public void setAttrValueId(Long attrValueId) {
+        this.attrValueId = attrValueId;
+    }
+
+    public Long getApplyRegionId() {
+        return applyRegionId;
+    }
+
+    public void setApplyRegionId(Long applyRegionId) {
+        this.applyRegionId = applyRegionId;
+    }
+}
